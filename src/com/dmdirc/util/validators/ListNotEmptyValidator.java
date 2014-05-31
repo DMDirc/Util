7db512ea68a5370eb7d8c4c1f82b02aa978ad37c
Greg Holmes <greg@dmdirc.com>
2014-05-31 19:37:17 +0100

    Add list not empty validator.
    Change-Id: Ibc0ff0c4ba26dfe7ed577e44bffbb8f20982ba5c

---
+++ src/com/dmdirc/util/validators/ListNotEmptyValidator.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2006-2014 DMDirc Developers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.util.validators;
+
+import java.util.List;
+
+/**
+ * Validator checking the list is not empty.
+ *
+ * @param <T> List type
+ */
+public class ListNotEmptyValidator<T> implements Validator<List<T>> {
+
+    /**
+     * Serialisation version.
+     */
+    private static final long serialVersionUID = 1;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public ValidationResponse validate(final List<T> object) {
+        if (object == null || object.isEmpty()) {
+            return new ValidationResponse("List cannot be an empty.");
+        } else {
+            return new ValidationResponse();
+        }
+    }
+
+}
