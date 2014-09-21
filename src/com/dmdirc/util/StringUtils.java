b97cc362ef20cf550dcf7aaeb5c67de601e07c6c
Chris Smith <chris@dmdirc.com>
2014-09-21 14:28:38 +0100

    Add StringUtils class + tests.
    These are from the TextPane, but seem fairly general purpose.
    
    Change-Id: Ic119eb461a365b4f7050715c6bc0abd5c034e57c

---
+++ src/com/dmdirc/util/StringUtils.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2006-2014 DMDirc Developers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.util;
+
+/**
+ * Utilities for dealing with strings.
+ */
+public final class StringUtils {
+
+    private StringUtils() {
+        // Shouldn't be instansiated.
+    }
+
+    /**
+     * Returns the indexes for the word surrounding the index in the specified string.
+     *
+     * @param text  Text to get word from
+     * @param index Index to get surrounding word
+     *
+     * @return An array containing two elements: the index of the first character of the word, and
+     * the index of the first character beyond the end of the word. If the specified index is not
+     * contained within a word (i.e., is whitespace) then 0,0 is returned.
+     */
+    public static int[] indiciesOfWord(final CharSequence text, final int index) {
+        final int start = indexOfStartOfWord(text, index);
+        final int end = indexOfEndOfWord(text, index);
+
+        if (start > end) {
+            return new int[]{0, 0};
+        }
+
+        return new int[]{start, end};
+
+    }
+
+    /**
+     * Returns the start index for the word surrounding the index in the specified string.
+     *
+     * @param text  Text to get word from
+     * @param index Index to get surrounding word
+     *
+     * @return Start index of the word surrounding the index
+     */
+    public static int indexOfStartOfWord(final CharSequence text, final int index) {
+        int start = index;
+
+        // Traverse backwards
+        while (start > 0 && start < text.length() && text.charAt(start) != ' ') {
+            start--;
+        }
+        if (start + 1 < text.length() && text.charAt(start) == ' ') {
+            start++;
+        }
+
+        return start;
+    }
+
+    /**
+     * Returns the end index for the word surrounding the index in the specified string.
+     *
+     * @param text  Text to get word from
+     * @param index Index to get surrounding word
+     *
+     * @return End index of the word surrounding the index
+     */
+    public static int indexOfEndOfWord(final CharSequence text, final int index) {
+        int end = index;
+
+        // And forwards
+        while (end < text.length() && end >= 0 && text.charAt(end) != ' ') {
+            end++;
+        }
+
+        return end;
+    }
+
+}
---
+++ test/com/dmdirc/util/StringUtilsTest.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2006-2014 DMDirc Developers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public class StringUtilsTest {
+
+    @Test
+    public void testIndexOfStartOfWord() {
+        final String input = "abc 123 def";
+        assertEquals(0, StringUtils.indexOfStartOfWord(input, 0));
+        assertEquals(0, StringUtils.indexOfStartOfWord(input, 1));
+        assertEquals(0, StringUtils.indexOfStartOfWord(input, 2));
+
+        assertEquals(4, StringUtils.indexOfStartOfWord(input, 3));
+        assertEquals(4, StringUtils.indexOfStartOfWord(input, 4));
+        assertEquals(4, StringUtils.indexOfStartOfWord(input, 5));
+        assertEquals(4, StringUtils.indexOfStartOfWord(input, 6));
+
+        assertEquals(8, StringUtils.indexOfStartOfWord(input, 7));
+        assertEquals(8, StringUtils.indexOfStartOfWord(input, 8));
+        assertEquals(8, StringUtils.indexOfStartOfWord(input, 9));
+        assertEquals(8, StringUtils.indexOfStartOfWord(input, 10));
+    }
+
+    @Test
+    public void testIndexOfEndOfWord() {
+        final String input = "abc 123 def";
+        assertEquals(3, StringUtils.indexOfEndOfWord(input, 0));
+        assertEquals(3, StringUtils.indexOfEndOfWord(input, 1));
+        assertEquals(3, StringUtils.indexOfEndOfWord(input, 2));
+        assertEquals(3, StringUtils.indexOfEndOfWord(input, 3));
+
+        assertEquals(7, StringUtils.indexOfEndOfWord(input, 4));
+        assertEquals(7, StringUtils.indexOfEndOfWord(input, 5));
+        assertEquals(7, StringUtils.indexOfEndOfWord(input, 6));
+        assertEquals(7, StringUtils.indexOfEndOfWord(input, 7));
+
+        assertEquals(11, StringUtils.indexOfEndOfWord(input, 8));
+        assertEquals(11, StringUtils.indexOfEndOfWord(input, 9));
+        assertEquals(11, StringUtils.indexOfEndOfWord(input, 10));
+    }
+
+    @Test
+    public void testIndicesOfWord() {
+        final String input = "abc 123 def";
+        assertArrayEquals(new int[]{0, 3}, StringUtils.indiciesOfWord(input, 0));
+        assertArrayEquals(new int[]{0, 3}, StringUtils.indiciesOfWord(input, 1));
+        assertArrayEquals(new int[]{0, 3}, StringUtils.indiciesOfWord(input, 2));
+
+        assertArrayEquals(new int[]{0, 0}, StringUtils.indiciesOfWord(input, 3));
+
+        assertArrayEquals(new int[]{4, 7}, StringUtils.indiciesOfWord(input, 4));
+        assertArrayEquals(new int[]{4, 7}, StringUtils.indiciesOfWord(input, 5));
+        assertArrayEquals(new int[]{4, 7}, StringUtils.indiciesOfWord(input, 6));
+
+        assertArrayEquals(new int[]{0, 0}, StringUtils.indiciesOfWord(input, 7));
+
+        assertArrayEquals(new int[]{8, 11}, StringUtils.indiciesOfWord(input, 8));
+        assertArrayEquals(new int[]{8, 11}, StringUtils.indiciesOfWord(input, 9));
+        assertArrayEquals(new int[]{8, 11}, StringUtils.indiciesOfWord(input, 10));
+    }
+
+}
