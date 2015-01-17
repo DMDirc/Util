47101951d4c50a0bed19e5d2ad015fd187fd4ec9
Greg Holmes <greg@dmdirc.com>
2015-01-17 00:59:37 +0000

    Add a TriConsumer.

---
+++ src/com/dmdirc/util/functional/TriConsumer.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2006-2015 DMDirc Developers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.util.functional;
+
+import java.util.function.Consumer;
+
+/**
+ * Represents an operation that accepts three input arguments and returns no result.  This is the
+ * three-arity specialization of {@link Consumer}.
+ *
+ * @param <T> The type of the first argument to the operation
+ * @param <U> The type of the second argument to the operation
+ * @param <V> The type of the third argument to the operation
+ *
+ * @see Consumer
+ */
+@FunctionalInterface
+public interface TriConsumer<T, U, V> {
+
+    /**
+     * Performs this operation on the given arguments.
+     *
+     * @param t The first input argument
+     * @param u The second input argument
+     * @param v The third input argument
+     */
+    void accept(T t, U u, V v);
+}
